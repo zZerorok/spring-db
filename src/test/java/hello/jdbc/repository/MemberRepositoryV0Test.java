3807c3a821401b8fba9784230b1bf8d62790a5b0
youngrok <yr951103@gmail.com>
2025-03-01 00:08:14 +0900

    test: 회원 수정 테스트 코드

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -23,5 +23,10 @@ class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        // update money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
     }
 }
